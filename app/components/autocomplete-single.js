c7e32bf819cde17498d338309047a27aeffef8d7
Josh Worden <solocommand@gmail.com>
2018-03-01 15:48:36 -0600

    Updated pagination support
    Ref cygnusb2b/fortnight-graph#18

--- app/components/autocomplete-single.js
+++ app/components/autocomplete-single.js
@@ -1,17 +1,46 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import { inject } from '@ember/service';
 import { task, timeout } from 'ember-concurrency';
+import ComponentQueryManager from 'ember-apollo-client/mixins/component-query-manager';
 
-export default Component.extend({
-  modelQuery: inject(),
+import advertiserQuery from 'fortnight/gql/queries/search-advertisers';
+import publisherQuery from 'fortnight/gql/queries/search-publishers';
+
+export default Component.extend(ComponentQueryManager, {
+  errorProcessor: inject(),
 
   type: null,
+  field: 'name',
   selected: null,
   placeholder: null,
 
+  _query: computed('type', function() {
+    const type = this.get('type');
+    switch (type) {
+      case 'advertiser':
+        return advertiserQuery;
+      case 'publisher':
+        return publisherQuery;
+    }
+    this.get('errorProcessor').show(new Error(`The model type ${type} is not searchable.`));
+  }),
+
+  _pagination: computed('type', 'term', 'page', function() {
+    // @todo this should actually be paginated, probably
+    return { first: 20 };
+  }),
+
   search: task(function* (term) {
+    const pagination = this.get('_pagination');
+    const field = this.get('field');
+    const search = { typeahead: { field, term } };
+    const variables = { pagination, search };
+    const query = this.get('_query');
     yield timeout(600);
-    const criteria = { $text: { $search: term } };
-    return this.get('modelQuery').execute(this.get('type'), criteria, 25, null, null, false)
+    return this.apollo.watchQuery({ query, variables }, 'searchAdvertisers.edges')
+      .then(r => r.map(i => i.node))
+      .catch(e => this.get('errorProcessor').show(e))
+    ;
   }),
 });
