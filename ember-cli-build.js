9cabb8ca088e445d8b8fed1a289930a811f24528
Josh Worden <solocommand@gmail.com>
2018-03-07 10:34:23 -0600

    Update bootstrap, bootswatch, icons

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,24 +1,26 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const Funnel = require('broccoli-funnel');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  // Bootstrap JS and source maps.
+  app.import('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js');
+  app.import('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map', { destDir: 'assets' });
+  app.import('node_modules/bootstrap/dist/css/bootstrap.min.css.map', { destDir: 'assets' });
 
-  return app.toTree();
+  app.import('node_modules/ionicons/css/ionicons.min.css');
+  app.import('node_modules/ionicons/css/ionicons.min.css.map', { destDir: 'assets' });
+
+  var ionicons = new Funnel('node_modules/ionicons/fonts', {
+    srcDir: '/',
+    include: [ '*.*' ],
+    destDir: '/fonts'
+  });
+
+  return app.toTree(ionicons);
 };
