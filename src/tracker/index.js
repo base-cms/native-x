50556c1d6f5d6a157de061b6ed28c16fef6ad2a3
Jacob Bare <jacob@limit0.io>
2018-03-22 14:29:15 -0500

    Cleanup

--- src/tracker/index.js
+++ src/tracker/index.js
@@ -4,12 +4,39 @@ const domain = 'https://fortnight.as3.io';
 
 export default class Tracker {
   constructor(options = {}) {
-    this.opts = assign({ domain }, options);
-    console.info('Tracker initialized.', this.opts);
+    this.options = assign({ domain }, options);
     this.commands = {};
   }
-  execute(command, options = {}) {
-    const opts = assign({}, options);
-    console.info('execute', command, opts, this.commands[command]);
+
+  /**
+   * Excutes a command on the tracker.
+   *
+   * @param {string} command
+   * @param {...object} args
+   */
+  execute(command, ...args) {
+    if (this.commands[command]) {
+      this.commands[command](...args);
+    }
+  }
+
+  /**
+   * Configures the domain name for sending events.
+   *
+   * @return string
+   */
+  get domain() {
+    if (!this.options.domain) return '';
+    return `${this.options.domain.replace(/\/+$/, '')}`;
+  }
+
+  /**
+   * Creates a URL using the provided endoint with the configured domain.
+   *
+   * @param {string} endpoint
+   * @return {string}
+   */
+  createUrl(endpoint) {
+    return `${this.domain}/${endpoint.replace(/^\/+/)}`;
   }
 }
