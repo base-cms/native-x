7aef8fcb066ad6d75d1b4bfad971b79cfa0ad980
Josh Worden <solocommand@gmail.com>
2018-03-22 15:58:41 -0500

    rename deleted route

--- tests/unit/routes/login-test.js
+++ tests/unit/routes/login-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:docs', 'Unit | Route | docs', {
+moduleFor('route:login', 'Unit | Route | login', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
