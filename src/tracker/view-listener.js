cf5fa66c74375651725cb1c123ea4daa8cb0d277
Jacob Bare <jacob@limit0.io>
2018-03-25 10:46:07 -0500

    Add mutation handling

--- src/tracker/view-listener.js
+++ src/tracker/view-listener.js
@@ -1,4 +1,4 @@
-import { domReady } from '../utils';
+import { domReady, assign } from '../utils';
 
 require('intersection-observer');
 
@@ -21,11 +21,25 @@ function isVisible(threshold, record) {
  * @todo We mant to explore including a MutationObserver polyfill.
  */
 export default class ViewListener {
-  constructor(tracker) {
+  constructor(tracker, options = {}) {
     // Disable if the browser does not support the required features.
     if (!(window.IntersectionObserver && window.MutationObserver)) return;
 
+    const defaults = {
+      trackOnce: true,
+      threshold: 0.25,
+      rootMargin: '0px',
+    };
+    this.opts = assign(defaults, options);
+
+    // Bind methods.
     this.handleIntersection = this.handleIntersection.bind(this);
+    this.handleMutations = this.handleMutations.bind(this);
+    this.handleAddedElement = this.handleAddedElement.bind(this);
+    this.handleRemovedElement = this.handleRemovedElement.bind(this);
+
+    this.mutationObserver = null;
+    this.intersectionObserver = null;
 
     this.elementMap = {};
     this.tracker = tracker;
@@ -41,37 +55,100 @@ export default class ViewListener {
     return ids;
   }
 
+  observeElements() {
+    // Setup intersection observer.
+    if (!this.intersectionObserver) {
+      this.intersectionObserver = new IntersectionObserver(this.handleIntersection, {
+        rootMargin: this.opts.rootMargin,
+        threshold: this.opts.threshold,
+      });
+      const ids = ViewListener.getElementIds();
+      ids.forEach((id) => {
+        const element = document.getElementById(id);
+        this.elementMap[id] = element;
+        if (element) this.intersectionObserver.observe(element);
+      });
+    }
+
+    // Setup mutation observer.
+    if (!this.mutationObserver) {
+      this.mutationObserver = new MutationObserver(this.handleMutations);
+      this.mutationObserver.observe(document.body, {
+        childList: true,
+        subtree: true,
+      });
+    }
+
+    // https://bugs.chromium.org/p/chromium/issues/detail?id=612323
+    requestAnimationFrame(() => {});
+  }
+
+  unobserveAllElements() {
+    this.intersectionObserver.disconnect();
+    this.intersectionObserver = null;
+
+    this.mutationObserver.disconnect();
+    this.mutationObserver = null;
+
+    this.elementMap = {};
+  }
+
   handleIntersection(records) {
-    const toRemove = [];
     for (let i = 0; i < records.length; i += 1) {
       const record = records[i];
-      if (isVisible(0.25, record)) {
+      if (isVisible(this.opts.threshold, record)) {
         const { id } = record.target;
         this.sendEvent(id);
-        toRemove.push(id);
+        if (this.opts.trackOnce) {
+          this.handleRemovedElement(id);
+        }
       }
     }
   }
 
-  sendEvent(id) {
-    const element = document.getElementById(id);
-    this.tracker.execute('event', 'view', { id: element.id }, { transport: 'beacon' });
-  }
-
-  observeElements() {
-    const ids = ViewListener.getElementIds();
-
-    const observer = new IntersectionObserver(this.handleIntersection, {
-      rootMargin: '0px',
-      threshold: 0.25,
-    });
-
-    ids.forEach((id) => {
-      const element = this.elementMap[id] || document.getElementById(id);
-      if (element) {
-        observer.observe(element);
+  handleMutations(mutations) {
+    mutations.forEach((mutation) => {
+      const { addedNodes, removedNodes } = mutation;
+      for (let r = 0; r < removedNodes.length; r += 1) {
+        const removed = removedNodes[r];
+        this.walkTree(removed, this.handleRemovedElement);
+      }
+      for (let a = 0; a < addedNodes.length; a += 1) {
+        const added = addedNodes[a];
+        this.walkTree(added, this.handleAddedElement);
       }
     });
+  }
+
+  handleAddedElement(id) {
+    const element = this.elementMap[id] || document.getElementById(id);
+    if (element) {
+      this.intersectionObserver.observe(element);
+    }
+    this.elementMap[id] = element;
+  }
+
+  handleRemovedElement(id) {
+    const element = this.elementMap[id] || document.getElementById(id);
+    if (element) {
+      this.intersectionObserver.unobserve(element);
+    }
+    this.elementMap[id] = null;
+  }
+
+  walkTree(node, callback) {
+    if (node.nodeType === 1 && node.id.indexOf('fortnight-') === 0) {
+      callback(node.id);
+    }
+    for (let i = 0; i < node.childNodes.length; i += 1) {
+      const child = node.childNodes[i];
+      this.walkTree(child, callback);
+    }
+  }
+
+  sendEvent(id) {
+    const element = this.elementMap[id] || document.getElementById(id);
+    this.tracker.execute('event', 'view', { id: element.id }, { transport: 'beacon' });
   }
 
   destroy() {
