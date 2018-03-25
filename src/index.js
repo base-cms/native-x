0921a2f2517cf149a9fe90e6238620b3e80b207f
Jacob Bare <jacob@limit0.io>
2018-03-25 12:17:34 -0500

    Remove legacy tracker
    Prefer to a/b test using both scripts seperately…
    e.g. include both `/lib/0.1.3` and the new initialization script

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import app from './app';
-import legacy from './legacy';
-
-// Execute legacy tracker. Remove once tested.
-legacy();
 
 // Find the command queue in the `window` object.
 const WINDOW_VAR_NAME = 'FortnightObject';
