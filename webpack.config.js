48ba5e2a28e90b1cde117dbc021cbd2baf9448c6
Jacob Bare <jacob@limit0.io>
2018-03-27 10:53:44 -0500

    Do not compile modules with babel

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,12 @@ module.exports = function(env) {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['env'],
+              presets: [
+                ['env', {
+                  modules: false,
+                  debug: true,
+                }],
+              ],
             },
           },
         },
