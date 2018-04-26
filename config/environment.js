2cb5217bb324910bdd486737737ae495094a283d
Jacob Bare <jacob@limit0.io>
2018-04-26 08:22:38 -0500

    Enable transition logging in dev

--- config/environment.js
+++ config/environment.js
@@ -30,8 +30,8 @@ module.exports = function(environment) {
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    ENV.APP.LOG_TRANSITIONS = true;
+    ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
     // ENV.apollo.apiURL = 'http://localhost:8100/graph';
   }
