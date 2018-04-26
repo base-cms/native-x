bfcd03a044b3dfdf3c6928f01dc73d6706bfaefa
Jacob Bare <jacob@limit0.io>
2018-04-26 09:24:11 -0500

    Remove error processor service in favor of graph

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,11 +1,7 @@
 import Route from '@ember/routing/route';
-import { inject } from '@ember/service';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Route.extend(ApplicationRouteMixin, {
-  apollo: inject(),
-  errorProcessor: inject(),
-
   actions: {
     linkTo(name) {
       this.transitionTo(name);
