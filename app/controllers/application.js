753c2efe7957561a712e1779ffc527edfd241713
Jacob Bare <jacob@limit0.io>
2018-04-26 12:37:17 -0500

    Add update current user profile

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,10 +2,14 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   isChangePasswordOpen: false,
+  isUpdateProfileOpen: false,
 
   actions: {
     displayChangePassword() {
       this.set('isChangePasswordOpen', true);
+    },
+    displayUpdateProfile() {
+      this.set('isUpdateProfileOpen', true);
     },
   },
 
