1d9c5078c2f527de3b60bb35c08261f9eeff966b
Jacob Bare <jacob@limit0.io>
2018-04-26 22:06:36 -0500

    Update placement edit/delete

--- app/routes/placement/create.js
+++ app/routes/placement/create.js
@@ -1,4 +1,5 @@
 import Route from '@ember/routing/route';
+import { get } from '@ember/object';
 import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 import ActionMixin from 'fortnight/mixins/action-mixin';
 
@@ -12,9 +13,10 @@ export default Route.extend(RouteQueryManager, ActionMixin, {
   actions: {
     async create({ name, publisher }) {
       this.startRouteAction();
-      const payload = { name, publisherId: publisher.id };
+      const payload = { name, publisherId: get(publisher || {}, 'id') };
       const variables = { input: { payload } };
       try {
+        if (!payload.publisherId) throw new Error('You must select a publisher to continue.');
         const response = await this.apollo.mutate({ mutation, variables }, 'createPlacement');
         this.get('notify').info('Placement successfully created.');
         return this.transitionTo('placement.edit', response.id);
--- app/routes/placement/edit.js
+++ app/routes/placement/edit.js
@@ -1,33 +1,36 @@
 import Route from '@ember/routing/route';
+import { get } from '@ember/object';
 import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
+import ActionMixin from 'fortnight/mixins/action-mixin';
 
 import query from 'fortnight/gql/queries/placement';
 import mutation from 'fortnight/gql/mutations/update-placement';
 
-export default Route.extend(RouteQueryManager, {
-
+export default Route.extend(RouteQueryManager, ActionMixin, {
   model({ id }) {
-    const resultKey = 'placement';
     const variables = { input: { id } };
-    return this.apollo.watchQuery({ query, variables }, resultKey);
-  },
-
-  renderTemplate() {
-    this.render();
-    this.render('placement.actions.edit', { outlet: 'actions', into: 'application' });
+    return this.apollo.watchQuery({ query, variables }, 'placement');
   },
 
   actions: {
-    update({ id, name, publisher }) {
-      const resultKey = 'updatePlacement';
-      const publisherId = publisher.id;
-      const payload = { name, publisherId };
+    async update({ id, name, publisher }) {
+      this.startRouteAction();
+      const payload = { name, publisherId: get(publisher || {}, 'id') };
       const variables = { input: { id, payload } };
-      const refetchQueries = ['placement', 'AllPlacements'];
-      return this.apollo.mutate({ mutation, variables, refetchQueries }, resultKey)
-        .then(() => this.get('notify').info('Placement saved.'))
-        .catch(e => this.get('errorProcessor').show(e))
-      ;
-    }
+      try {
+        if (!payload.publisherId) throw new Error('You must select a publisher to continue.');
+        await this.apollo.mutate({ mutation, variables }, 'updatePlacement');
+        this.get('notify').info('Placement successfully saved.');
+      } catch (e) {
+        this.get('graphErrors').show(e);
+      } finally {
+        this.endRouteAction();
+      }
+    },
+
+    async delete() {
+      this.get('notify').warning('Deleting objects is not yet supported.');
+    },
   }
-})
+});
+
