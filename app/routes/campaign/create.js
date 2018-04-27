89e5743ea901893b0036fb118f574d68acbceebd
Jacob Bare <jacob@limit0.io>
2018-04-27 10:26:35 -0500

    Add initial campaign create form

--- app/routes/campaign/create.js
+++ app/routes/campaign/create.js
@@ -1,48 +1,65 @@
 import Route from '@ember/routing/route';
+import { get } from '@ember/object';
 import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
-import { inject } from '@ember/service';
-import { getProperties } from '@ember/object';
+import ActionMixin from 'fortnight/mixins/action-mixin';
 
 import mutation from 'fortnight/gql/mutations/create-campaign';
 
-export default Route.extend(RouteQueryManager, {
-  errorProcessor: inject(),
-
+export default Route.extend(RouteQueryManager, ActionMixin, {
   model() {
-    const { givenName, familyName, email } = getProperties(this.get('user.model'), ['givenName', 'familyName', 'email']);
-    const user = {
-      name: `${givenName} ${familyName}`,
-      value: email,
-    };
     return {
-      status: 'Draft',
+      // status: 'Draft',
+      isNew: true,
       externalLinks: [],
-      notify: {
-        internal: [user],
-        external: [],
-      },
     };
-  },
-
-  renderTemplate() {
-    this.render();
-    this.render('campaign.actions.create', { outlet: 'actions', into: 'application' });
   },
 
   actions: {
-    create({ name, description, advertiser, url, externalLinks }) {
-      const advertiserId = advertiser.id;
-      const links = externalLinks.map(({ label, url }) => Object.assign({}, { label, url }));
-      const payload = { url, name, description, advertiserId, externalLinks: links };
+    /**
+     *
+     * @param {object} fields
+     */
+    async create({ name, description, advertiser, url, externalLinks }) {
+      this.startRouteAction();
+      const payload = {
+        name,
+        description,
+        advertiserId: get(advertiser || {}, 'id'),
+        url,
+        externalLinks,
+      };
       const variables = { input: { payload } };
-      const resultKey = 'createCampaign';
-      const refetchQueries = ['campaign', 'allCampaigns'];
-      return this.apollo.mutate({ mutation, variables, refetchQueries }, resultKey)
-        .then(response => this.transitionTo('campaign.edit', response.id))
-        .catch(e => this.get('errorProcessor').show(e))
-      ;
+      try {
+        if (!payload.publisherId) throw new Error('You must select a publisher to continue.');
+        const response = await this.apollo.mutate({ mutation, variables }, 'createCampaign');
+        this.get('notify').info('Campaign successfully created.');
+        return this.transitionTo('campaign.edit', response.id);
+      } catch (e) {
+        this.get('graphErrors').show(e);
+      } finally {
+        this.endRouteAction();
+      }
     },
+
+    /**
+     *
+     */
     update() {
+      // Do nothing. Prevents auto-save on create.
     },
-  }
-})
+
+    // create({ name, description, advertiser, url, externalLinks }) {
+    //   const advertiserId = advertiser.id;
+    //   const links = externalLinks.map(({ label, url }) => Object.assign({}, { label, url }));
+    //   const payload = { url, name, description, advertiserId, externalLinks: links };
+    //   const variables = { input: { payload } };
+    //   const resultKey = 'createCampaign';
+    //   const refetchQueries = ['campaign', 'allCampaigns'];
+    //   return this.apollo.mutate({ mutation, variables, refetchQueries }, resultKey)
+    //     .then(response => this.transitionTo('campaign.edit', response.id))
+    //     .catch(e => this.get('errorProcessor').show(e))
+    //   ;
+    // },
+  },
+});
+
