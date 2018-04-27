a8822582e274fa7bf3503c0ef9ae34970e9d1dc3
Jacob Bare <jacob@limit0.io>
2018-04-27 12:55:29 -0500

    Update initial campaign create

--- app/routes/campaign/create.js
+++ app/routes/campaign/create.js
@@ -30,7 +30,6 @@ export default Route.extend(RouteQueryManager, ActionMixin, {
       };
       const variables = { input: { payload } };
       try {
-        if (!payload.publisherId) throw new Error('You must select a publisher to continue.');
         const response = await this.apollo.mutate({ mutation, variables }, 'createCampaign');
         this.get('notify').info('Campaign successfully created.');
         return this.transitionTo('campaign.edit', response.id);
@@ -40,26 +39,6 @@ export default Route.extend(RouteQueryManager, ActionMixin, {
         this.endRouteAction();
       }
     },
-
-    /**
-     *
-     */
-    update() {
-      // Do nothing. Prevents auto-save on create.
-    },
-
-    // create({ name, description, advertiser, url, externalLinks }) {
-    //   const advertiserId = advertiser.id;
-    //   const links = externalLinks.map(({ label, url }) => Object.assign({}, { label, url }));
-    //   const payload = { url, name, description, advertiserId, externalLinks: links };
-    //   const variables = { input: { payload } };
-    //   const resultKey = 'createCampaign';
-    //   const refetchQueries = ['campaign', 'allCampaigns'];
-    //   return this.apollo.mutate({ mutation, variables, refetchQueries }, resultKey)
-    //     .then(response => this.transitionTo('campaign.edit', response.id))
-    //     .catch(e => this.get('errorProcessor').show(e))
-    //   ;
-    // },
   },
 });
 
