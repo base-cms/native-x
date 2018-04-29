da07d83d0d36a893e8081aa0eb02126959a44624
Jacob Bare <jacob@limit0.io>
2018-04-29 15:21:53 -0500

    Move create action to controller

---
+++ app/controllers/campaign/create.js
@@ -0,0 +1,37 @@
+import Controller from '@ember/controller';
+import { get } from '@ember/object';
+import { inject } from '@ember/service';
+import ActionMixin from 'fortnight/mixins/action-mixin';
+
+import mutation from 'fortnight/gql/mutations/create-campaign';
+
+export default Controller.extend(ActionMixin, {
+  apollo: inject(),
+
+  actions: {
+    /**
+     *
+     * @param {object} fields
+     */
+    async create({ name, description, advertiser, url, externalLinks }) {
+      this.startAction();
+      const payload = {
+        name,
+        description,
+        advertiserId: get(advertiser || {}, 'id'),
+        url,
+        externalLinks,
+      };
+      const variables = { input: { payload } };
+      try {
+        const response = await this.get('apollo').mutate({ mutation, variables }, 'createCampaign');
+        this.get('notify').info('Campaign successfully created.');
+        return this.transitionTo('campaign.edit', response.id);
+      } catch (e) {
+        this.get('graphErrors').show(e);
+      } finally {
+        this.endAction();
+      }
+    },
+  },
+});
--- app/routes/campaign/create.js
+++ app/routes/campaign/create.js
@@ -1,44 +1,12 @@
 import Route from '@ember/routing/route';
-import { get } from '@ember/object';
-import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
-import ActionMixin from 'fortnight/mixins/action-mixin';
 
-import mutation from 'fortnight/gql/mutations/create-campaign';
-
-export default Route.extend(RouteQueryManager, ActionMixin, {
+export default Route.extend({
   model() {
     return {
       // status: 'Draft',
       isNew: true,
       externalLinks: [],
     };
-  },
-
-  actions: {
-    /**
-     *
-     * @param {object} fields
-     */
-    async create({ name, description, advertiser, url, externalLinks }) {
-      this.startRouteAction();
-      const payload = {
-        name,
-        description,
-        advertiserId: get(advertiser || {}, 'id'),
-        url,
-        externalLinks,
-      };
-      const variables = { input: { payload } };
-      try {
-        const response = await this.apollo.mutate({ mutation, variables }, 'createCampaign');
-        this.get('notify').info('Campaign successfully created.');
-        return this.transitionTo('campaign.edit', response.id);
-      } catch (e) {
-        this.get('graphErrors').show(e);
-      } finally {
-        this.endRouteAction();
-      }
-    },
   },
 });
 
