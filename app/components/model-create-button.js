0c2b4780fa1249b2500a8e79e8e96702542eb7a1
Jacob Bare <jacob@limit0.io>
2018-04-30 12:48:53 -0500

    Adjust create button to accept left or right

--- app/components/model-create-button.js
+++ app/components/model-create-button.js
@@ -2,7 +2,7 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: 'button',
-  classNames: ['btn', 'btn-lg', 'btn-success', 'create', 'fixed-bottom', 'float-right'],
+  classNames: ['btn', 'btn-lg', 'btn-success', 'fixed-bottom'],
   attributeBindings: ['title'],
   icon: '',
   title: 'Create New Record',
