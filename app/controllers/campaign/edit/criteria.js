357ae5da3bcc9d151faa2472eab4dd0715662a5c
Jacob Bare <jacob@limit0.io>
2018-04-30 11:46:48 -0500

    Update criteria placements and key/values

--- app/controllers/campaign/edit/criteria.js
+++ app/controllers/campaign/edit/criteria.js
@@ -2,19 +2,27 @@ import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 import moment from 'moment';
 
-export default Controller.extend({
-  startMax: computed.reads('model.criteria.end'),
-  startMin: moment().startOf('day'),
-  endMin: computed.reads('model.criteria.start'),
-  endMax: null,
+import mutation from 'fortnight/gql/mutations/campaign/set-criteria';
 
-  isSaving: false,
-  hasSaved: false,
-  saveMessage: computed('isSaving', 'hasSaved', function() {
-    const { isSaving, hasSaved } = this.getProperties(['isSaving', 'hasSaved']);
-    if (isSaving) return 'Your changes are being saved.';
-    if (hasSaved) return 'Your changes have been saved.';
-    return 'Changes made on this tab are saved automatically.';
-  }),
+export default Controller.extend({
+  actions: {
+    update() {
+      console.info('update!', this.get('model'));
+    },
+  },
+
+  // startMax: computed.reads('model.criteria.end'),
+  // startMin: moment().startOf('day'),
+  // endMin: computed.reads('model.criteria.start'),
+  // endMax: null,
+
+  // isSaving: false,
+  // hasSaved: false,
+  // saveMessage: computed('isSaving', 'hasSaved', function() {
+  //   const { isSaving, hasSaved } = this.getProperties(['isSaving', 'hasSaved']);
+  //   if (isSaving) return 'Your changes are being saved.';
+  //   if (hasSaved) return 'Your changes have been saved.';
+  //   return 'Changes made on this tab are saved automatically.';
+  // }),
 
 });
--- app/routes/campaign/edit/criteria.js
+++ app/routes/campaign/edit/criteria.js
@@ -1,22 +1,18 @@
 import Route from '@ember/routing/route';
 import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
+import ActionMixin from 'fortnight/mixins/action-mixin';
 
 import { get } from '@ember/object';
 import { isPresent } from '@ember/utils';
 import moment from 'moment';
 
-import query from 'fortnight/gql/queries/campaign-criteria';
-import mutation from 'fortnight/gql/mutations/set-campaign-criteria';
+import query from 'fortnight/gql/queries/campaign/criteria';
 
-export default Route.extend(RouteQueryManager, {
-
-  hasSaved: false,
-
+export default Route.extend(RouteQueryManager, ActionMixin, {
   model() {
     const { id } = this.modelFor('campaign.edit');
-    const resultKey = 'campaign';
     const variables = { input: { id } };
-    return this.get('apollo').watchQuery({ query, variables }, resultKey);
+    return this.get('apollo').watchQuery({ query, variables }, 'campaign');
   },
 
   afterModel(model) {
@@ -29,48 +25,26 @@ export default Route.extend(RouteQueryManager, {
     if (!get(model, 'criteria')) model.criteria = criteria;
   },
 
-  renderTemplate() {
-    this.render();
-    this.render('campaign.actions.edit.criteria', { outlet: 'actions', into: 'application' });
-  },
-
-  doUpdate() {
-    const controller = this.controllerFor('campaign.edit.criteria');
-    controller.set('isSaving', true);
-    const model = this.modelFor('campaign.edit.criteria');
-    const { id, criteria } = model;
-    const { start, end, kvs, placements } = criteria;
-    const keyValues = kvs.map(({ key, value }) => {
-      return { key, value };
-    });
-    const campaignId = id;
-    const placementIds = placements.map(p => p.id);
-    const startDate = moment(start).format('x');
-    const endDate = isPresent(end) ? moment(end).format('x') : null;
-    const payload = { start: startDate, end: endDate, placementIds, kvs: keyValues };
-    const variables = { input: { campaignId, payload } };
-    const resultKey = 'setCampaignCriteria';
-    const refetchQueries = ['campaign', 'campaignCriteria'];
-    return this.get('apollo').mutate({ mutation, variables, refetchQueries }, resultKey)
-      .then(() => controller.setProperties({ isSaving: false, hasSaved: true }))
-      .catch(e => this.get('errorProcessor').show(e))
-    ;
-  },
-
-  actions: {
-    update() {
-      return this.doUpdate();
-    },
-    didTransition() {
-      this.set('hasSaved', false);
-    },
-    willTransition(transition) {
-      if (this.get('hasSaved')) return true;
-      transition.abort();
-      this.doUpdate()
-        .then(() => this.set('hasSaved', true))
-        .then(() => transition.retry())
-      ;
-    },
-  },
-})
+  // doUpdate() {
+  //   const controller = this.controllerFor('campaign.edit.criteria');
+  //   controller.set('isSaving', true);
+  //   const model = this.modelFor('campaign.edit.criteria');
+  //   const { id, criteria } = model;
+  //   const { start, end, kvs, placements } = criteria;
+  //   const keyValues = kvs.map(({ key, value }) => {
+  //     return { key, value };
+  //   });
+  //   const campaignId = id;
+  //   const placementIds = placements.map(p => p.id);
+  //   const startDate = moment(start).format('x');
+  //   const endDate = isPresent(end) ? moment(end).format('x') : null;
+  //   const payload = { start: startDate, end: endDate, placementIds, kvs: keyValues };
+  //   const variables = { input: { campaignId, payload } };
+  //   const resultKey = 'setCampaignCriteria';
+  //   const refetchQueries = ['campaign', 'campaignCriteria'];
+  //   return this.get('apollo').mutate({ mutation, variables, refetchQueries }, resultKey)
+  //     .then(() => controller.setProperties({ isSaving: false, hasSaved: true }))
+  //     .catch(e => this.get('errorProcessor').show(e))
+  //   ;
+  // },
+});
