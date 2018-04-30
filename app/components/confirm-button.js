2ec86fe336d451b98c8496646c198a8e3e6c5007
Jacob Bare <jacob@limit0.io>
2018-04-30 08:36:14 -0500

    Add support for classes and additional icon

--- app/components/confirm-button.js
+++ app/components/confirm-button.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   tagName: 'button',
@@ -9,11 +10,30 @@ export default Component.extend({
   type: 'button',
 
   icon: '',
+  confirmIcon: '',
+  iconClass: null,
+
   label: 'Action',
   confirmLabel: 'You Sure?',
+  labelClass: null,
+
   onConfirm: null,
 
   hasConfirmed: false,
+
+  _icon: computed('icon', 'confirmIcon', 'hasConfirmed', function() {
+    const icon = this.get('icon');
+    const confirmIcon = this.get('confirmIcon');
+    if (!this.get('hasConfirmed')) return icon;
+    return confirmIcon ? confirmIcon : icon;
+  }),
+
+  _label: computed('label', 'confirmLabel', 'hasConfirmed', function() {
+    const label = this.get('label');
+    const confirmLabel = this.get('confirmLabel');
+    if (!this.get('hasConfirmed')) return label;
+    return confirmLabel ? confirmLabel : icon;
+  }),
 
   click() {
     if (this.get('hasConfirmed')) {
