224c13a8d05ad3c08f6768ddf4d480236f3323b7
Jacob Bare <jacob@limit0.io>
2018-04-30 12:42:05 -0500

    Ensure campaign criteria refetchs

--- app/routes/campaign/edit/criteria.js
+++ app/routes/campaign/edit/criteria.js
@@ -9,7 +9,7 @@ export default Route.extend(RouteQueryManager, {
   model() {
     const { id } = this.modelFor('campaign.edit');
     const variables = { input: { id } };
-    return this.get('apollo').watchQuery({ query, variables }, 'campaign');
+    return this.get('apollo').watchQuery({ query, variables, refetchPolicy: 'network-only' }, 'campaign');
   },
 
   afterModel(model) {
