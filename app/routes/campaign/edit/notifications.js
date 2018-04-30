ab763aec1194042cc6efff6a8f28536ce07ef99c
Jacob Bare <jacob@limit0.io>
2018-04-30 10:50:45 -0500

    Update campaign notifications/contacts

--- app/routes/campaign/edit/notifications.js
+++ app/routes/campaign/edit/notifications.js
@@ -1,18 +1,12 @@
 import Route from '@ember/routing/route';
 import RouteQueryManager from "ember-apollo-client/mixins/route-query-manager";
 
-import query from 'fortnight/gql/queries/campaign-notifications';
+import query from 'fortnight/gql/queries/campaign/contacts';
 
 export default Route.extend(RouteQueryManager, {
   model() {
     const { id } = this.modelFor('campaign.edit');
-    const resultKey = 'campaign';
     const variables = { input: { id } };
-    return this.apollo.watchQuery({ query, variables }, resultKey);
-  },
-
-  renderTemplate() {
-    this.render();
-    this.render('campaign.actions.edit.notifications', { outlet: 'actions', into: 'application' });
+    return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, 'campaign');
   },
 });
