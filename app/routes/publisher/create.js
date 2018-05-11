9e7f6b747a9e9adad5bf22b555e957516bc5500d
Jacob Bare <jacob@limit0.io>
2018-05-11 09:56:16 -0500

    Move publisher create action to controller

--- app/routes/publisher/create.js
+++ app/routes/publisher/create.js
@@ -1,28 +1,7 @@
 import Route from '@ember/routing/route';
-import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
-import ActionMixin from 'fortnight/mixins/action-mixin';
 
-import mutation from 'fortnight/gql/mutations/create-publisher';
-
-export default Route.extend(RouteQueryManager, ActionMixin, {
+export default Route.extend({
   model() {
     return {};
   },
-
-  actions: {
-    async create({ name, logo }) {
-      this.startRouteAction();
-      const payload = { name, logo };
-      const variables = { input: { payload } };
-      try {
-        const response = await this.get('apollo').mutate({ mutation, variables }, 'createPublisher');
-        this.get('notify').success('Property successfully created.');
-        return this.transitionTo('publisher.edit', response.id);
-      } catch (e) {
-        this.get('graphErrors').show(e);
-      } finally {
-        this.endRouteAction();
-      }
-    }
-  }
 })
