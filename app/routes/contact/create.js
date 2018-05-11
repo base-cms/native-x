008dd91e66ac8320c7b77c5aa3cd281b63d5f3cc
Jacob Bare <jacob@limit0.io>
2018-05-11 10:26:34 -0500

    Move contact create/update actions to controllers

--- app/routes/contact/create.js
+++ app/routes/contact/create.js
@@ -1,29 +1,8 @@
 import Route from '@ember/routing/route';
-import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
-import ActionMixin from 'fortnight/mixins/action-mixin';
 
-import mutation from 'fortnight/gql/mutations/create-contact';
-
-export default Route.extend(RouteQueryManager, ActionMixin, {
+export default Route.extend({
   model() {
     return {};
-  },
-
-  actions: {
-    async create({ email, givenName, familyName }) {
-      this.startRouteAction();
-      const payload = { email, givenName, familyName };
-      const variables = { input: { payload } };
-      try {
-        const response = await this.get('apollo').mutate({ mutation, variables }, 'createContact');
-        this.get('notify').info('Contact successfully created.');
-        return this.transitionTo('contact.edit', response.id);
-      } catch (e) {
-        this.get('graphErrors').show(e);
-      } finally {
-        this.endRouteAction();
-      }
-    }
   },
 });
 
--- app/routes/contact/edit.js
+++ app/routes/contact/edit.js
@@ -1,35 +1,13 @@
 import Route from '@ember/routing/route';
 import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
-import ActionMixin from 'fortnight/mixins/action-mixin';
 
 import query from 'fortnight/gql/queries/contact';
-import mutation from 'fortnight/gql/mutations/update-contact';
 
-export default Route.extend(RouteQueryManager, ActionMixin, {
+export default Route.extend(RouteQueryManager, {
   model({ id }) {
     const variables = { input: { id } };
     return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, 'contact');
   },
-
-  actions: {
-    async update({ id, email, givenName, familyName }) {
-      this.startRouteAction();
-      const payload = { email, givenName, familyName };
-      const variables = { input: { id, payload } };
-      try {
-        await this.get('apollo').mutate({ mutation, variables }, 'updateContact');
-        this.get('notify').info('Contact successfully saved.');
-      } catch (e) {
-        this.get('graphErrors').show(e);
-      } finally {
-        this.endRouteAction();
-      }
-    },
-
-    async delete() {
-      this.get('notify').warning('Deleting objects is not yet supported.');
-    },
-  }
 });
 
 
