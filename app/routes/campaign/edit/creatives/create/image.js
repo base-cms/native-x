59a7b4bc20dd73fd06dcaa63fc04279fecccc3c7
Jacob Bare <jacob@limit0.io>
2018-05-12 13:55:10 -0500

    Ensure image create cannot be directly accessed

--- app/routes/campaign/edit/creatives/create/image.js
+++ app/routes/campaign/edit/creatives/create/image.js
@@ -1,6 +1,16 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  beforeModel() {
+    const controller = this.controllerFor('campaign.edit.creatives.create');
+    const form = controller.get('detailsForm');
+    if (!form) {
+      // Signifies that the image route is being directly accessed.
+      // Redirect to the main create page.
+      return this.transitionTo('campaign.edit.creatives.create.index');
+    }
+  },
+
   renderTemplate() {
     const controller = this.controllerFor('campaign.edit.creatives.create');
     this.render('campaign.edit.creatives.create.image', {
