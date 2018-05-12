317469cba2db7f0b035272adb54f930baa51124d
Jacob Bare <jacob@limit0.io>
2018-05-12 11:44:44 -0500

    Move campaign creative component to partial

--- app/components/campaign-creative/image-src.js
+++ app/components/campaign-creative/image-src.js
@@ -2,25 +2,18 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
-  classNames: [ 'card', 'mnh-100' ],
+  tagName: '',
 
   src: computed('image.{src,focalPoint.x,focalPoint.y}', function() {
-    const { src, focalPoint } = this.get('image');
+    const { src, focalPoint } = this.get('image') || {};
     const { x, y } = focalPoint || {};
     if (!src) return 'https://via.placeholder.com/640x360?text=No%20Image%20Uploaded+';
     return `${src}?w=300&h=169&crop=focalpoint&fit=crop&dpr=1&fp-x=${x}&fp-y=${y}`
   }),
 
   hasSrc: computed('image.src', function() {
-    const { src } = this.get('image');
+    const { src } = this.get('image') || {};
     if (src) return true;
     return false;
   }),
-
-  init() {
-    this._super(...arguments);
-
-    const { id, name, url, title, teaser, image } = this.get('creative');
-    this.setProperties({ id, name, url, title, teaser, image: image || {} });
-  },
 });
