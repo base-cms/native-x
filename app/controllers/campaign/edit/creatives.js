c14df51dcc89df20e3b45d91f2e55531fc6d4854
Jacob Bare <jacob@limit0.io>
2018-05-14 09:20:14 -0500

    Add creative remove support

--- app/controllers/campaign/edit/creatives.js
+++ app/controllers/campaign/edit/creatives.js
@@ -1,3 +1,27 @@
 import Controller from '@ember/controller';
+import ActionMixin from 'fortnight/mixins/action-mixin';
+import { inject } from '@ember/service';
 
-export default Controller.extend({});
+import removeCreative from 'fortnight/gql/mutations/campaign/remove-creative';
+
+export default Controller.extend(ActionMixin, {
+  apollo: inject(),
+
+  actions: {
+    async remove(creativeId) {
+      this.startAction();
+      const campaignId = this.get('model.id');
+
+      const mutation = removeCreative;
+      const variables = { input: { campaignId, creativeId } };
+      const refetchQueries = ['CampaignCreatives'];
+      try {
+        await this.get('apollo').mutate({ mutation, variables, refetchQueries }, 'removeCampaignCreative');
+      } catch (e) {
+        this.get('graphErrors').show(e)
+      } finally {
+        this.endAction();
+      }
+    },
+  },
+});
