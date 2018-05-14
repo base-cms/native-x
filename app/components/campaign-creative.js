8dae2355ceb93ceceb860e8534acf7f3058ce0d9
Jacob Bare <jacob@limit0.io>
2018-05-14 11:12:38 -0500

    Move individual creative back to component
    Allows for actions to be split by creative instead of the entire campaign

---
+++ app/components/campaign-creative.js
@@ -0,0 +1,56 @@
+import Component from '@ember/component';
+import ActionMixin from 'fortnight/mixins/action-mixin';
+import { inject } from '@ember/service';
+import { scheduleOnce, run } from '@ember/runloop';
+
+import removeCreative from 'fortnight/gql/mutations/campaign/remove-creative';
+import creativeStatus from 'fortnight/gql/mutations/campaign/creative-status';
+
+export default Component.extend(ActionMixin, {
+  classNames: ['card', 'mnh-100'],
+
+  campaignId: null,
+  creativeId: null,
+
+  apollo: inject(),
+
+  init() {
+    this._super(...arguments);
+
+    // Ensure the action has ended.
+    // Fixes issue with calling set on destroyed object after status is updated.
+    this.endAction();
+  },
+
+  actions: {
+    async remove() {
+      this.startAction();
+      const { campaignId, creativeId } = this.getProperties('campaignId', 'creativeId');
+
+      const mutation = removeCreative;
+      const variables = { input: { campaignId, creativeId } };
+      const refetchQueries = ['CampaignCreatives'];
+      try {
+        await this.get('apollo').mutate({ mutation, variables, refetchQueries }, 'removeCampaignCreative');
+      } catch (e) {
+        this.get('graphErrors').show(e)
+      } finally {
+        this.endAction();
+      }
+    },
+
+    async updateStatus(status) {
+      this.startAction();
+      const { campaignId, creativeId } = this.getProperties('campaignId', 'creativeId');
+
+      const mutation = creativeStatus;
+      const variables = { input: { campaignId, creativeId, status } };
+      try {
+        await this.get('apollo').mutate({ mutation, variables }, 'campaignCreativeStatus');
+      } catch (e) {
+        this.get('graphErrors').show(e);
+        this.endAction();
+      }
+    },
+  },
+});
