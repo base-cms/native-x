cbca815c4f9bd2db57a91e31a511f435fab1eb20
Jacob Bare <jacob@limit0.io>
2018-05-18 12:24:45 -0500

    Update publisher search and autocomplete

--- app/routes/publisher/index.js
+++ app/routes/publisher/index.js
@@ -3,9 +3,13 @@ import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 import { getObservable } from 'ember-apollo-client';
 
 import query from 'fortnight/gql/queries/all-publishers';
+import search from 'fortnight/gql/queries/search-publishers';
 
 export default Route.extend(RouteQueryManager, {
   queryParams: {
+    phrase: {
+      refreshModel: true
+    },
     first: {
       refreshModel: true
     },
@@ -20,14 +24,31 @@ export default Route.extend(RouteQueryManager, {
     },
   },
 
-  model({ first, after, sortBy, ascending }) {
+  search(phrase, pagination) {
     const controller = this.controllerFor(this.get('routeName'));
+    const variables = { pagination, phrase };
+    const resultKey = 'searchPublishers';
+    controller.set('resultKey', resultKey);
+    return this.get('apollo').watchQuery({ query: search, variables, fetchPolicy: 'network-only' }, resultKey)
+      .then((result) => {
+        controller.set('observable', getObservable(result));
+        return result;
+      }).catch(e => this.get('graphErrors').show(e))
+    ;
+  },
 
+  model({ first, after, sortBy, ascending, phrase }) {
+    const controller = this.controllerFor(this.get('routeName'));
     const pagination = { first, after };
+
+    if (phrase) {
+      return this.search(phrase, pagination);
+    }
     const sort = { field: sortBy, order: ascending ? 1 : -1 };
     const variables = { pagination, sort };
+    const resultKey = 'allPublishers';
     if (!sortBy) delete variables.sort.field;
-    return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, 'allPublishers')
+    return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, resultKey)
       .then((result) => {
         controller.set('observable', getObservable(result));
         return result;
