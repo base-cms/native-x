7c908108e37da1e332b820e3b6f03e1a3969158c
Jacob Bare <jacob@limit0.io>
2018-06-07 14:48:01 -0500

    Get initial story create working with advertiser

--- app/controllers/story/create.js
+++ app/controllers/story/create.js
@@ -1,8 +1,9 @@
 import Controller from '@ember/controller';
 import { inject } from '@ember/service';
+import { get } from '@ember/object';
 import ActionMixin from 'fortnight/mixins/action-mixin';
 
-import mutation from 'fortnight/gql/mutations/create-story';
+import mutation from 'fortnight/gql/mutations/story/create';
 
 export default Controller.extend(ActionMixin, {
   apollo: inject(),
@@ -12,9 +13,12 @@ export default Controller.extend(ActionMixin, {
      *
      * @param {object} fields
      */
-    async create({ title, teaser, body }) {
+    async create({ title, advertiser }) {
       this.startAction();
-      const payload = { title, teaser, body };
+      const payload = {
+        title,
+        advertiserId: get(advertiser || {}, 'id'),
+      };
       const variables = { input: { payload } };
       try {
         const response = await this.get('apollo').mutate({ mutation, variables }, 'createStory');
