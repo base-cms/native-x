f93e0bb5d4998f0d930f6593b38447c85eedd25c
Brian Miller <mills_77_77@hotmail.com>
2018-06-07 11:03:57 -0500

    dont need this

--- app/controllers/story/edit.js
+++ app/controllers/story/edit.js
@@ -1,6 +1,5 @@
 import Controller from '@ember/controller';
 import { inject } from '@ember/service';
-import { get } from '@ember/object';
 import { all } from 'rsvp';
 import ActionMixin from 'fortnight/mixins/action-mixin';
 
