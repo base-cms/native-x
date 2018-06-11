cac71886fe8c099cc82499e7bf5dccb8dd1ffc3e
Jacob Bare <jacob@limit0.io>
2018-06-11 10:30:48 -0500

    Setup initial primary image story templates

---
+++ app/controllers/story/edit/primary-image.js
@@ -0,0 +1,50 @@
+import Controller from '@ember/controller';
+import { inject } from '@ember/service';
+import { all } from 'rsvp';
+import { get } from '@ember/object';
+import moment from 'moment';
+import ActionMixin from 'fortnight/mixins/action-mixin';
+
+import updateStory from 'fortnight/gql/mutations/story/update';
+
+export default Controller.extend(ActionMixin, {
+  apollo: inject(),
+
+  actions: {
+    /**
+     *
+     * @param {object} fields
+     */
+    async setPrimaryImage({ primaryImage }) {
+      this.startAction();
+      console.info(primaryImage);
+      this.endAction();
+      // const promises = [];
+      // const payload = {
+      //   title,
+      //   teaser,
+      //   body,
+      //   advertiserId: get(advertiser || {}, 'id'),
+      //   publishedAt: publishedAt ? publishedAt.valueOf() : null,
+      // };
+      // const variables = { input: { id, payload } };
+      // const mutation = updateStory;
+
+      // try {
+      //   await all(promises);
+      //   await this.get('apollo').mutate({ mutation, variables }, 'updateStory');
+      // } catch (e) {
+      //   this.get('graphErrors').show(e);
+      // } finally {
+      //   this.endAction();
+      // }
+    },
+
+    /**
+     *
+     */
+    async delete() {
+      this.get('notify').warning('Deleting objects is not yet supported.');
+    },
+  },
+});
