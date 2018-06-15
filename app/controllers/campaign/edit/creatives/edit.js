2a0e5a4394fb8c0b7eb60312a1dda8a646e30e89
Jacob Bare <jacob@limit0.io>
2018-06-15 10:46:07 -0500

    Add support for updating creative image

--- app/controllers/campaign/edit/creatives/edit.js
+++ app/controllers/campaign/edit/creatives/edit.js
@@ -3,6 +3,7 @@ import { inject } from '@ember/service';
 import ActionMixin from 'fortnight/mixins/action-mixin';
 
 import campaignCreativeDetails from 'fortnight/gql/mutations/campaign/creative-details';
+import campaignCreativeImage from 'fortnight/gql/mutations/campaign/creative-image';
 
 export default Controller.extend(ActionMixin, {
   apollo: inject(),
@@ -11,14 +12,25 @@ export default Controller.extend(ActionMixin, {
   actions: {
     /**
      * Handles the image once uploaded.
-     * In this case, related the newly uploaded image to the creative.
+     * In this case, relates the newly uploaded image to the creative.
      *
      * @param {object} image
      * @param {string} image.id
-     * @param {string} image.link
      */
-    async handleCreativeImage({ id, link }) {
-      // set(this.get('model'), 'image', { id, src: link });
+    async handleCreativeImage({ id }) {
+      const creativeId = this.get('model.id');
+      const campaignId = this.get('campaignId');
+
+      const mutation = campaignCreativeImage;
+      const input = { campaignId, creativeId, imageId: id };
+      const variables = { input };
+      try {
+        await this.get('apollo').mutate({ mutation, variables }, 'campaignCreativeImage');
+      } catch (e) {
+        this.get('graphErrors').show(e);
+      } finally {
+        this.endAction();
+      }
     },
 
     /**
