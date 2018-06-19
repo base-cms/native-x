785516199156e1e210bb6ef1d6b61729a977993a
Jacob Bare <jacob@limit0.io>
2018-06-19 13:46:04 -0500

    Query by hash for campaign materials

--- app/routes/portal/campaigns/manage.js
+++ app/routes/portal/campaigns/manage.js
@@ -1,8 +1,13 @@
 import Route from '@ember/routing/route';
+import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 
-export default Route.extend({
+import query from 'fortnight/gql/queries/campaign/hash';
+
+export default Route.extend(RouteQueryManager, {
   model({ campaign_hash }) {
-    console.info('campaign_hash', campaign_hash);
+    const advertiserId = this.modelFor('portal').get('id');
+    const variables = { input: { hash: campaign_hash, advertiserId } };
+    return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, 'campaignHash');
   }
 });
 
