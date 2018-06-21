878b3ea98825265caaa0a102129f94b46d3dc324
Jacob Bare <jacob@limit0.io>
2018-06-21 14:14:30 -0500

    Autosave mcf on url change

--- app/controllers/portal/campaigns/manage/materials.js
+++ app/controllers/portal/campaigns/manage/materials.js
@@ -1,8 +1,45 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
+import { inject } from '@ember/service';
+import { isURL } from 'validator';
+import ActionMixin from 'fortnight/mixins/action-mixin';
 
-export default Controller.extend({
+import mutation from 'fortnight/gql/mutations/campaign/url';
+
+export default Controller.extend(ActionMixin, {
+  apollo: inject(),
+
   canRemoveCreatives: computed('model.campaign.creatives.length', function() {
     return this.get('model.campaign.creatives.length') > 1;
   }),
+
+  actions: {
+    validateUrl(instance, form) {
+      const url = form.elements['campaign-url'];
+      if (!url) return;
+
+      const { value } = url;
+      if (!value) return;
+
+      const isValid = isURL(value, {
+        protocols: ['http', 'https'],
+        require_protocol: true,
+      })
+      url.setCustomValidity(isValid ? '' : 'The provided URL is invalid.');
+    },
+
+    async updateUrl({ url }) {
+      this.startAction();
+      const campaignId = this.get('model.campaign.id');
+      const variables = { input: { campaignId, url } };
+
+      try {
+        await this.get('apollo').mutate({ mutation, variables }, 'campaignUrl');
+      } catch (e) {
+        this.get('graphErrors').show(e);
+      } finally {
+        this.endAction();
+      }
+    },
+  },
 });
