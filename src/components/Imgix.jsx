2caf399a098c6a505085602f3693e983a7160da0
Brandon Krigbaum <brandonbk@gmail.com>
2018-06-25 15:14:42 -0500

    Updated props to use src instead of path & host. Added temporary support for div elements.

--- src/components/Imgix.jsx
+++ src/components/Imgix.jsx
@@ -12,37 +12,46 @@ const params = [
 ];
 
 const Imgix = (props) => {
-  const { host, path } = props;
+  let { src } = props;
 
   const qs = params.map((k) => {
     const v = props[k];
     return (v !== null && v !== undefined) ? `${encodeURIComponent(k)}=${encodeURIComponent(v)}` : '';
   }).filter(kv => kv !== '').join('&');
 
-  const original = `${host}/${path}`;
-  const src = qs ? `${original}?${qs}` : original;
-
+  src = qs ? `${src}?${qs}` : src;
+  
   let img;
-  if (path) {
-    img = <img className={props.className} src={src} alt={props.alt} title={props.title} />;
+  if (src) {
+    if (props.tag == 'img') {
+      img = <img className={props.className} src={src} alt={props.alt} title={props.title} />;
+    } else if (props.tag == 'div') {
+      const divStyle = {
+        backgroundImage: `url(${src})`
+      }
+      img = <div className={props.className} style={divStyle}>{props.children}</div>;
+    }
+    
   }
+  
   return (
     img
   );
 };
 
 Imgix.defaultProps = {
-  host: 'https://base.imgix.net',
-  dpr: process.browser ? window.devicePixelRatio || 1 : 1,
+  //dpr: process.browser ? window.devicePixelRatio || 1 : 1,
+  dpr: 1,
   title: null,
   alt: null,
   className: null,
+  tag: 'img',
 };
 
 Imgix.propTypes = {
-  host: PropTypes.string.isRequired,
-  path: PropTypes.string,
+  src: PropTypes.string.isRequired,
   dpr: PropTypes.number,
+  tag: PropTypes.oneOf(['img', 'div']),
 };
 
 export default Imgix;
