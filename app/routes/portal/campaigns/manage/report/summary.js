a36d6aebd80e226c8da182081e596388b606222b
Josh Worden <solocommand@gmail.com>
2018-06-28 13:13:08 -0500

    Port over data query

---
+++ app/routes/portal/campaigns/manage/report/summary.js
@@ -0,0 +1,16 @@
+import Route from '@ember/routing/route';
+import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
+
+import query from 'fortnight/gql/queries/reports/campaign/summary';
+
+export default Route.extend(RouteQueryManager, {
+
+  model() {
+    const model = this.modelFor('portal.campaigns.manage');
+    const { hash } = model.campaign;
+    const advertiserId = model.advertiser.id;
+    const variables = { input: { hash, advertiserId } };
+    return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, 'campaignSummaryReport');
+  }
+})
+
