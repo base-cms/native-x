039af73e301e83cb3ad6973626877f9fbbe0c3db
Josh Worden <solocommand@gmail.com>
2018-06-29 11:16:13 -0500

    create imgix-image component

---
+++ app/components/imgix-image.js
@@ -0,0 +1,19 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  tagName: 'img',
+  classNames: [ 'img-fluid' ],
+  attributeBindings: [ 'src' ],
+
+  width: 640,
+  height: 360,
+
+  src: computed('image.{src,focalPoint.x,focalPoint.y}', 'width', 'height', function() {
+    const { src, focalPoint } = this.get('image');
+    const { x, y } = focalPoint;
+    const { width, height } = this.getProperties(['width', 'height']);
+    return `${src}?w=${width}&h=${height}&crop=focalPoint&fit=crop&dpr=1&fp-x=${x}&fp-y=${y}`;
+  }),
+
+});
