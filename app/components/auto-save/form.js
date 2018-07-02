ee18c37facabbd620bf76b85217fffe697a0470f
Jacob Bare <jacob@limit0.io>
2018-07-02 10:57:42 -0500

    Adjust how form is submitted

--- app/components/auto-save/form.js
+++ app/components/auto-save/form.js
@@ -3,17 +3,16 @@ import Component from '@ember/component';
 export default Component.extend({
   tagName: 'form',
   attributeBindings: ['novalidate'],
-  // classNameBindings: ['wasValidated:was-validated'],
   novalidate: true,
 
   shouldAutosave: true,
 
   submit(event) {
-    if (this.get('shouldAutosave')) {
-      // When autosave is enabled, prevent the regular form submission.
-      // This allows for per-field actions without saving the entire form.
-      event.preventDefault();
-      event.stopPropagation();
+    event.preventDefault();
+    event.stopPropagation();
+    if (!this.get('shouldAutosave')) {
+      // Only submit the entire form when auto-save is disabled.
+      this.get('on-submit')();
     }
   },
 });
