4f6e6e5668f02d70325fc0f64d8101aae0bac0eb
Josh Worden <solocommand@gmail.com>
2018-07-12 11:38:23 -0500

    only use eslint on dev
    https://github.com/zeit/next.js/issues/2184

--- next.config.js
+++ next.config.js
@@ -2,15 +2,17 @@ module.exports = {
   distDir: '../.next/build',
 
   webpack: (config, { dev }) => {
-    config.module.rules.push({
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      enforce: 'pre',
-      loader: 'eslint-loader',
-      options: {
-        emitWarning: dev,
-      },
-    });
+    if (dev) {
+      config.module.rules.push({
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: dev,
+        },
+      });
+    }
     return config;
   },
 };
