691cc35b7da0c29d203d6103ae6cbcf9eca3673b
Brian Miller <mills_77_77@hotmail.com>
2018-07-17 10:22:25 -0500

    add background on header overlay on scroll up

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -21,9 +21,12 @@ const PageWrapper = props => (
     <style jsx global>{`
         nav.fixed-top {
           transition: top 0.1s ease-in-out;
+          transition: background-color 0.1s ease-in-out;
           background-color: transparent;
         }
-
+        .fixed-top.nav-down {
+          background-color: rgba(0,0,0,0.75);
+        }
         .fixed-top.nav-up {
           top: -71px;
         }
