4a6163f72dccd84b2864c36fcab94447183481c9
Jacob Bare <jacob@limit0.io>
2018-07-19 13:36:45 -0500

    Cleanup internal/external contacts on advertiser create

--- app/routes/manage/advertiser/create.js
+++ app/routes/manage/advertiser/create.js
@@ -3,7 +3,10 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return {
-      notify: {}
+      notify: {
+        internal: [],
+        external: [],
+      }
     };
   },
 });
