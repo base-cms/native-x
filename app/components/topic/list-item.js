33d748da07a5b495964430e78fc614a4b6464039
Jacob Bare <jacob@limit0.io>
2018-07-23 16:00:47 -0500

    Add topic list-item component

---
+++ app/components/topic/list-item.js
@@ -0,0 +1,9 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  tagName: '',
+  class: 'list-group-item list-group-item-action flex-column align-items-start',
+
+  item: null,
+  routeName: null,
+});
