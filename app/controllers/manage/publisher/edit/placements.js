7f0ca7a3034a743c0d3602d627b59afd69e9b1ec
Jacob Bare <jacob@limit0.io>
2018-07-24 11:35:24 -0500

    Add publisher placement sort/limit

--- app/controllers/manage/publisher/edit/placements.js
+++ app/controllers/manage/publisher/edit/placements.js
@@ -1,3 +1,13 @@
-import Controller from '@ember/controller';
+import ListController from 'fortnight/controllers/manage/abstract-list';
 
-export default Controller.extend({});
+export default ListController.extend({
+  init() {
+    this._super(...arguments);
+    this.set('sortOptions', [
+      { key: 'createdAt', label: 'Created' },
+      { key: 'updatedAt', label: 'Updated' },
+      { key: 'name', label: 'Name' },
+    ]);
+    this.set('sortBy', 'updatedAt');
+  },
+});
