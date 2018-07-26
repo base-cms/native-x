0489b70f497c3deeeb1b3799483776c06339e9b9
Jacob Bare <jacob@limit0.io>
2018-07-26 13:54:06 -0500

    Add new campaign status/requires

--- app/components/campaign-status.js
+++ app/components/campaign-status.js
@@ -1,20 +1,24 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 
-
 export default Component.extend({
   tagName: 'span',
-  classNameBindings: ['statusText'],
+  classNameBindings: ['color'],
 
-  statusText: computed('value', function() {
-    switch (this.get('value')) {
+  status: null,
+
+  color: computed('status', function() {
+    switch (this.get('status')) {
       case 'Draft':
-        return 'text-warning';
-      case 'Ready':
+        return 'text-muted';
+      case 'Running':
         return 'text-success';
+      case 'Scheduled':
+        return 'text-info';
+      case 'Incomplete':
+        return 'text-warning';
       default:
         return 'text-muted';
     }
   }),
-
 });
