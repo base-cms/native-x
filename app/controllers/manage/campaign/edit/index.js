2e6e3f0d433438831509f2e2ad5ddc39554a49e5
Jacob Bare <jacob@limit0.io>
2018-07-27 10:02:20 -0500

    Add campaign delete

--- app/controllers/manage/campaign/edit/index.js
+++ app/controllers/manage/campaign/edit/index.js
@@ -4,11 +4,15 @@ import { get } from '@ember/object';
 import ActionMixin from 'fortnight/mixins/action-mixin';
 
 import mutation from 'fortnight/gql/mutations/campaign/update';
+import deleteCampaign from 'fortnight/gql/mutations/campaign/delete';
 
 export default Controller.extend(ActionMixin, {
   apollo: inject(),
 
   actions: {
+    /**
+     *
+     */
     async update({ id, name, description, advertiser, story, url, externalLinks }) {
       this.startAction();
       const payload = {
@@ -22,6 +26,24 @@ export default Controller.extend(ActionMixin, {
       const variables = { input: { id, payload } };
       try {
         await this.get('apollo').mutate({ mutation, variables }, 'updateCampaign');
+      } catch (e) {
+        this.get('graphErrors').show(e);
+      } finally {
+        this.endAction();
+      }
+    },
+
+    /**
+     * Deletes the campaign
+     */
+    async delete() {
+      this.startAction();
+      const id = this.get('model.id');
+      const variables = { input: { id } };
+      const mutation = deleteCampaign;
+      try {
+        await this.get('apollo').mutate({ mutation, variables }, 'deleteCampaign');
+        return this.transitionToRoute('manage.campaign.index');
       } catch (e) {
         this.get('graphErrors').show(e);
       } finally {
