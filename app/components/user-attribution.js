2e0a095b36ef8d508753d6a09d30b6af7f893d34
Jacob Bare <jacob@limit0.io>
2018-07-27 12:14:02 -0500

    Adjust user attribution and add to campaign list

--- app/components/user-attribution.js
+++ app/components/user-attribution.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  tagName: 'small',
+  classNames: ['d-block'],
   format: 'MMM Do, YYYY @ h:mma',
   label: 'Created',
 
