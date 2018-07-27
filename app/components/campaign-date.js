a580e2e32bea74d204352d81d5e89d4fe705ba06
Jacob Bare <jacob@limit0.io>
2018-07-27 08:31:14 -0500

    Add past/future labels to campaign dates

--- app/components/campaign-date.js
+++ app/components/campaign-date.js
@@ -1,8 +1,22 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import moment from 'moment';
 
 export default Component.extend({
   format: 'MMM Do, YYYY',
-  label: 'Starts',
+  futureLabel: 'Starts',
+  pastLabel: 'Started',
 
   date: null,
+
+  isInPast: computed('date', function() {
+    const date = this.get('date');
+    if (!date) return false;
+    return moment(date).isBefore(new Date());
+  }).readOnly(),
+
+  label: computed('futureLabel', 'pastLabel', 'isInPast', function() {
+    if (this.get('isInPast')) return this.get('pastLabel');
+    return this.get('futureLabel');
+  }).readOnly(),
 });
