ae85f36e288c30f9d06b97c99ff775b04afa32bb
Jacob Bare <jacob@limit0.io>
2018-08-01 10:10:25 -0500

    Add user edit

---
+++ app/controllers/manage/user/edit.js
@@ -0,0 +1,54 @@
+import Controller from '@ember/controller';
+import { inject } from '@ember/service';
+import ActionMixin from 'fortnight/mixins/action-mixin';
+
+import updateUser from 'fortnight/gql/mutations/user/update';
+import deleteUser from 'fortnight/gql/mutations/contact/delete';
+
+export default Controller.extend(ActionMixin, {
+  apollo: inject(),
+
+  actions: {
+    /**
+     *
+     */
+    async update() {
+      this.startAction();
+      const {
+        id,
+        email,
+        givenName,
+        familyName,
+      } = this.get('model');
+
+      const payload = { email, givenName, familyName };
+      const variables = { input: { id, payload } };
+      try {
+        await this.get('apollo').mutate({ mutation: updateUser, variables }, 'updateUser');
+      } catch (e) {
+        this.get('graphErrors').show(e);
+      } finally {
+        this.endAction();
+      }
+    },
+
+    /**
+     * Deletes the user
+     *
+     */
+    async delete() {
+      // this.startAction();
+      // const id = this.get('model.id');
+      // const variables = { input: { id } };
+      // const mutation = deleteUser;
+      // try {
+      //   await this.get('apollo').mutate({ mutation, variables }, 'deleteUser');
+      //   await this.transitionToRoute('manage.contact.index');
+      // } catch (e) {
+      //   this.get('graphErrors').show(e);
+      // } finally {
+      //   this.endAction();
+      // }
+    },
+  },
+});
