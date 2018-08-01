1ee531bba0b84f7a6e17b986b655ed697826eab4
Jacob Bare <jacob@limit0.io>
2018-08-01 10:16:51 -0500

    Add user undelete

--- app/controllers/manage/user/edit.js
+++ app/controllers/manage/user/edit.js
@@ -4,6 +4,7 @@ import ActionMixin from 'fortnight/mixins/action-mixin';
 
 import updateUser from 'fortnight/gql/mutations/user/update';
 import deleteUser from 'fortnight/gql/mutations/user/delete';
+import undeleteUser from 'fortnight/gql/mutations/user/undelete';
 
 export default Controller.extend(ActionMixin, {
   apollo: inject(),
@@ -43,6 +44,25 @@ export default Controller.extend(ActionMixin, {
       const mutation = deleteUser;
       try {
         await this.get('apollo').mutate({ mutation, variables }, 'deleteUser');
+        await this.transitionToRoute('manage.user.index');
+      } catch (e) {
+        this.get('graphErrors').show(e);
+      } finally {
+        this.endAction();
+      }
+    },
+
+    /**
+     * Restores (undeletes) the user
+     *
+     */
+    async undelete() {
+      this.startAction();
+      const id = this.get('model.id');
+      const variables = { input: { id } };
+      const mutation = undeleteUser;
+      try {
+        await this.get('apollo').mutate({ mutation, variables }, 'undeleteUser');
         await this.transitionToRoute('manage.user.index');
       } catch (e) {
         this.get('graphErrors').show(e);
