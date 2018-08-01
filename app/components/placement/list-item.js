2dfe13da900db88c9ceabd1056c0d85240df2eff
Jacob Bare <jacob@limit0.io>
2018-08-01 07:09:58 -0500

    Use own list item for publisher placements

--- app/components/placement/list-item.js
+++
@@ -1,9 +0,0 @@
-import Component from '@ember/component';
-
-export default Component.extend({
-  tagName: '',
-  class: 'list-group-item list-group-item-action flex-column align-items-start',
-
-  item: null,
-  routeName: null,
-});
