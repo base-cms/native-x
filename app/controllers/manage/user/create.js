fcd8dee63cdaf50d6900b2456152b36eabfb2495
Jacob Bare <jacob@limit0.io>
2018-08-01 09:42:50 -0500

    Add initial user create

---
+++ app/controllers/manage/user/create.js
@@ -0,0 +1,43 @@
+import Controller from '@ember/controller';
+import { inject } from '@ember/service';
+import ActionMixin from 'fortnight/mixins/action-mixin';
+
+import mutation from 'fortnight/gql/mutations/user/create';
+
+export default Controller.extend(ActionMixin, {
+  apollo: inject(),
+
+  actions: {
+    /**
+     *
+     * @param {object} fields
+     */
+    async create() {
+      this.startAction();
+      const {
+        email,
+        password,
+        confirmPassword,
+        givenName,
+        familyName,
+      } = this.get('model');
+
+      const payload = {
+        email,
+        password,
+        confirmPassword,
+        givenName,
+        familyName,
+      };
+      const variables = { input: { payload } };
+      try {
+        const response = await this.get('apollo').mutate({ mutation, variables }, 'createUser');
+        return this.transitionToRoute('manage.user.edit', response.id);
+      } catch (e) {
+        this.get('graphErrors').show(e);
+      } finally {
+        this.endAction();
+      }
+    }
+  },
+});
