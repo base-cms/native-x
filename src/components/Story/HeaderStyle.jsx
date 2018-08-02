725cdd6838318df06f2e8753797ee5cd983fabce
Brian Miller <mills_77_77@hotmail.com>
2018-08-02 13:59:37 -0500

    Added cname link to header

--- src/components/Story/HeaderStyle.jsx
+++ src/components/Story/HeaderStyle.jsx
@@ -16,7 +16,12 @@ export default () => (
       height: 80vh;
       background: url() center/cover no-repeat fixed;
     }
-
+    #publisher-bar {
+      box-shadow: 0 2px 5px 0 rgba(0,0,0,0.25), 0 2px 5px 0 rgba(0,0,0,0.25)
+    }
+    #branding-bar {
+      box-shadow: 0 16px 28px 0 rgba(0,0,0,0.22), 0 25px 55px 0 rgba(0,0,0,0.21);
+    }
     div.bg-imgIx:before {
       backgound: transparent;
       transition: background 0.5s linear;
@@ -37,6 +42,26 @@ export default () => (
       transition: background 1.5s ease-out;
     }
 
+    .arrow-left {
+      width: 0;
+      height: 0;
+      border-top: 5px solid transparent;
+      border-bottom: 5px solid transparent;
+
+      border-right:5px solid #fff;
+    }
+
+    .post-heading h1 {
+      color: #fff;
+      font-size: 5vmax;
+      font-family: 'Ubuntu', sans-serif;
+    }
+    @media screen and (min-width: 1200px) {
+      .post-heading h1 {
+        font-size: 60px;
+      }
+    }
+
     @media screen and (min-width: 1600px) {
       div.bg-imgIx:before {
         background: -moz-linear-gradient(left, rgba(0,0,0,1) 0%, rgba(0,0,0,0.25) 300px, rgba(0,0,0,0.25) calc(100% - 300px), rgba(0,0,0,1) 100%); /* FF3.6-15 */
@@ -47,12 +72,6 @@ export default () => (
         margin-left: auto;
         margin-right: auto;
       }
-    }
-
-    .post-heading h1 {
-      color: #fff;
-      font-size: 5vmax;
-      font-family: 'Ubuntu', sans-serif;
     }
   `}
   </style>
