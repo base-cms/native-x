09e4c06aaea05df8945b1848a1256e95f1bd287d
Brian Miller <mills_77_77@hotmail.com>
2018-08-02 07:54:47 -0500

    Adjusted Header to match rest of the user facing looks.

--- src/components/NavbarStyle.jsx
+++ src/components/NavbarStyle.jsx
@@ -5,7 +5,8 @@ export default () => (
     nav.fixed-top {
       transition: top 0.1s ease-in-out;
       transition: background-color 0.1s ease-in-out;
-      background-color: #000;
+      background-color: #fff;
+      box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.50), 0 2px 10px 0 rgba(0, 0, 0, 0.50);
     }
     .fixed-top.nav-down {
       background-color: rgba(0,0,0,0.75);
