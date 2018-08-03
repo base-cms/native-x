c3d2da2259869d3e61d64f5b5a28934e5144e7fd
Jacob Bare <jacob@limit0.io>
2018-08-03 10:48:55 -0500

    Add assign external contact to campaign

--- app/components/materials/contacts-builder/modal.js
+++ app/components/materials/contacts-builder/modal.js
@@ -1,9 +1,13 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { oneWay } from '@ember/object/computed';
 import ActionMixin from 'fortnight/mixins/action-mixin';
 import ComponentQueryManager from 'ember-apollo-client/mixins/component-query-manager';
 
+import campaignExternalContact from 'fortnight/gql/mutations/campaign/external-contact';
+
 export default Component.extend(ActionMixin, ComponentQueryManager, {
+  campaignId: null,
   contact: null,
   isOpen: false,
 
@@ -12,20 +16,49 @@ export default Component.extend(ActionMixin, ComponentQueryManager, {
     return true;
   }),
 
+  email: oneWay('contact.email'),
+  familyName: oneWay('contact.familyName'),
+  givenName: oneWay('contact.givenName'),
+
+  model: computed('email', 'familyName', 'givenName', function() {
+    return {
+      id: this.get('contact.id'),
+      email: this.get('email'),
+      givenName: this.get('givenName'),
+      familyName: this.get('familyName'),
+    };
+  }),
+
   init() {
     this._super(...arguments);
     const contact = this.get('contact');
-    if (!contact) {
-      this.set('contact', {});
-    } else {
-      // Clone the contact so it's one-way.
-      this.set('contact', { ...contact });
-    }
+    if (!contact) this.set('contact', {});
   },
 
   actions: {
-    async create() {
-      console.info('create');
+    async assign(contact) {
+      this.startAction();
+      const { id, email, givenName, familyName } = contact;
+      const payload = { email, givenName, familyName };
+      const input = {
+        contactId: id || null,
+        campaignId: this.get('campaignId'),
+        payload,
+      };
+      const variables = { input };
+      const refetchQueries = ['CampaignHash'];
+      try {
+        await this.get('apollo').mutate({ mutation: campaignExternalContact, variables, refetchQueries }, 'campaignExternalContact');
+        this.get('modal').send('hide');
+      } catch (e) {
+        this.get('graphErrors').show(e);
+      } finally {
+        this.endAction();
+      }
+    },
+
+    setModal(modal) {
+      this.set('modal', modal);
     },
   },
 });
