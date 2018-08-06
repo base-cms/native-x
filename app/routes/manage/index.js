5edba555f36f2715f6e378d81aca97056e842342
Jacob Bare <jacob@limit0.io>
2018-08-06 10:21:10 -0500

    Add daily campaign activity to dashboard

--- app/routes/manage/index.js
+++ app/routes/manage/index.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
+import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
+import moment from 'moment';
 
-export default Route.extend({});
+import query from 'fortnight/gql/queries/dashboard/daily-metrics';
 
+export default Route.extend(RouteQueryManager, {
+  model() {
+    const day = moment().startOf('day').valueOf();
+    const variables = { day };
+    return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, 'dailyCampaignMetrics');
+  },
+});
