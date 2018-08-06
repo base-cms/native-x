6e07d90668f0e66622fa109a5f10d5b4a7102a8e
Jacob Bare <jacob@limit0.io>
2018-08-06 12:26:56 -0500

    Add incomplete and running campaign queries

--- app/components/dashboard/running-campaigns.js
+++ app/components/dashboard/running-campaigns.js
@@ -1,8 +1,31 @@
 import Component from '@ember/component';
 import ComponentQueryManager from 'ember-apollo-client/mixins/component-query-manager';
 
+import query from 'fortnight/gql/queries/campaign/running';
+
 export default Component.extend(ComponentQueryManager, {
   classNames: ['card', 'border-0', 'z-depth-half'],
 
   isLoading: true,
+
+  didInsertElement() {
+    this.loadData();
+  },
+
+  async loadData() {
+    this.set('isLoading', true);
+
+    const pagination = { first: 25 };
+    const sort = { field: 'createdAt', order: -1 };
+    const variables = { pagination, sort };
+
+    try {
+      const data = await this.get('apollo').watchQuery({ query, variables }, 'runningCampaigns');
+      this.set('data', data);
+    } catch (e) {
+      this.get('graphErrors').show(e);
+    } finally {
+      this.set('isLoading', false);
+    }
+  },
 });
