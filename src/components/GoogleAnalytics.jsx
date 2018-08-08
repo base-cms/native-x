39cad5ec6d0f1a2bfd7fd06b7c25ed611dc0ab0d
Jacob Bare <jacob@limit0.io>
2018-08-08 12:09:34 -0500

    Update GA component to require props

--- src/components/GoogleAnalytics.jsx
+++ src/components/GoogleAnalytics.jsx
@@ -1,54 +1,51 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
-class GoogleAnalytics extends React.Component {
+/**
+ * Renders the global site script tag for gtag.js with Google Analytics.
+ *
+ * If no tracking ID is present, no script will be inserted.
+ * Also sets the accountKey globally.
+ *
+ * @see https://github.com/zeit/next.js/blob/master/examples/with-google-analytics
+ */
+const GoogleAnalytics = ({ trackingId, accountKey }) => {
+  if (!trackingId) return null;
   /**
+   * Global Site Tag (gtag.js) - Google Analytics
    *
    */
-  constructor(props) {
-    super(props);
-    const { GA_TRACKING_ID } = process.env;
-    this.state = {
-      trackingId: GA_TRACKING_ID,
-    };
-  }
+  return (
+    <Fragment>
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`} />
+      <script
+        dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            window.gtag('config', '${trackingId}', {
+              send_page_view: false,
+              custom_map: {
+                dimension1: 'accountKey',
+                dimension2: 'storyId'
+              },
+              accountKey: '${accountKey}'
+            });
+          `,
+        }}
+      />
+    </Fragment>
+  );
+};
 
-  /**
-   * Renders the global site script tag for gtag.js with
-   * Google Analytics.
-   *
-   * If no tracking ID is present, no script will be inserted.
-   *
-   * @see https://github.com/zeit/next.js/blob/master/examples/with-google-analytics
-   */
-  render() {
-    const { trackingId } = this.state;
-    if (!trackingId) return null;
-    /**
-     * Global Site Tag (gtag.js) - Google Analytics
-     *
-     */
-    return (
-      <Fragment>
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`} />
-        <script
-          dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              window.gtag('config', '${trackingId}', {
-                send_page_view: false,
-                custom_map: {
-                  dimension1: 'accountKey',
-                  dimension2: 'storyId',
-                },
-              });
-            `,
-          }}
-        />
-      </Fragment>
-    );
-  }
-}
+GoogleAnalytics.defaultProps = {
+  trackingId: '',
+};
+
+GoogleAnalytics.propTypes = {
+  trackingId: PropTypes.string,
+  accountKey: PropTypes.string.isRequired,
+};
 
 export default GoogleAnalytics;
