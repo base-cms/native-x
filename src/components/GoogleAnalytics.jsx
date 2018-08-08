3b385de628896d12d949d94242d0fbfbe65105f2
Jacob Bare <jacob@limit0.io>
2018-08-08 07:19:15 -0500

    Convert GA component to class

--- src/components/GoogleAnalytics.jsx
+++ src/components/GoogleAnalytics.jsx
@@ -1,34 +1,54 @@
 import React, { Fragment } from 'react';
 
-const GoogleAnalytics = () => {
-  const { GA_TRACKING_ID } = process.env;
-  if (!GA_TRACKING_ID) return null;
+class GoogleAnalytics extends React.Component {
+  /**
+   *
+   */
+  constructor(props) {
+    super(props);
+    const { GA_TRACKING_ID } = process.env;
+    this.state = {
+      trackingId: GA_TRACKING_ID,
+    };
+  }
 
   /**
-   * Global Site Tag (gtag.js) - Google Analytics
+   * Renders the global site script tag for gtag.js with
+   * Google Analytics.
+   *
+   * If no tracking ID is present, no script will be inserted.
+   *
    * @see https://github.com/zeit/next.js/blob/master/examples/with-google-analytics
    */
-  return (
-    <Fragment>
-      <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-      <script
-        dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            window.gtag('config', '${GA_TRACKING_ID}', {
-              send_page_view: false,
-              custom_map: {
-                dimension1: 'accountKey',
-                dimension2: 'storyId',
-              },
-            });
-          `,
-        }}
-      />
-    </Fragment>
-  );
-};
+  render() {
+    const { trackingId } = this.state;
+    if (!trackingId) return null;
+    /**
+     * Global Site Tag (gtag.js) - Google Analytics
+     *
+     */
+    return (
+      <Fragment>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`} />
+        <script
+          dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              window.gtag('config', '${trackingId}', {
+                send_page_view: false,
+                custom_map: {
+                  dimension1: 'accountKey',
+                  dimension2: 'storyId',
+                },
+              });
+            `,
+          }}
+        />
+      </Fragment>
+    );
+  }
+}
 
 export default GoogleAnalytics;
