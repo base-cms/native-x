515603e376d7c83853eea6cf14c858b022b60fc4
Jacob Bare <jacob@limit0.io>
2018-08-09 11:19:38 -0500

    Create story body component

--- src/components/StoryView.jsx
+++ src/components/StoryView.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Image from './StoryView/Header/Image';
 import Contents from './StoryView/Header/Contents';
+import Body from './StoryView/Body';
 import Style from './StoryView/Header/Style';
 
-const StoryView = ({ title, primaryImage }) => (
+const StoryView = ({
+  title,
+  teaser,
+  body,
+  primaryImage,
+}) => (
   <div className="story-wrapper">
     <Style />
     <div className="story-header">
@@ -12,15 +18,19 @@ const StoryView = ({ title, primaryImage }) => (
         <Contents title={title} />
       </Image>
     </div>
+    <Body teaser={teaser} body={body} />
   </div>
 );
 
 StoryView.defaultProps = {
+  teaser: '',
   primaryImage: {},
 };
 
 StoryView.propTypes = {
   title: PropTypes.string.isRequired,
+  teaser: PropTypes.string,
+  body: PropTypes.string.isRequired,
   primaryImage: PropTypes.shape({
     src: PropTypes.string,
     focalPoint: PropTypes.shape({
