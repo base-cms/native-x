d3fcb15ae9303c18ae1757ccfaaec6fdee6c6578
Jacob Bare <jacob@limit0.io>
2018-08-09 08:25:03 -0500

    Add story item primary image component

---
+++ src/components/StoryList/Item/PrimaryImage.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ImgixURL from '../../ImgixURL';
+
+
+const StoryListItemPrimaryImage = ({ src, focalPoint }) => {
+  if (!src) return null;
+  const { x, y } = focalPoint;
+  return (
+    <div className="d-flex flex-column my-auto mr-3">
+      <ImgixURL
+        src={src}
+        params={{
+          w: 150,
+          h: 150,
+          fit: 'crop',
+          crop: 'focalpoint',
+          'fp-x': x,
+          'fp-y': y,
+          dpr: 2,
+        }}
+      >
+        {url => <img src={url} alt="" width="150" height="150" /> }
+      </ImgixURL>
+    </div>
+  );
+};
+
+StoryListItemPrimaryImage.defaultProps = {
+  src: '',
+  focalPoint: {
+    x: 0.5,
+    y: 0.5,
+  },
+};
+
+StoryListItemPrimaryImage.propTypes = {
+  src: PropTypes.string,
+  focalPoint: PropTypes.shape({
+    x: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    y: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  }),
+};
+
+export default StoryListItemPrimaryImage;
