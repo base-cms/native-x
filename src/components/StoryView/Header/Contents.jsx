48d03f19e1c6d4aa7c2432ab63a36c2e08a702df
Jacob Bare <jacob@limit0.io>
2018-08-09 10:02:15 -0500

    Create story view header contents

---
+++ src/components/StoryView/Header/Contents.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const StoryViewHeaderContents = ({ title }) => (
+  <Container className="align-self-center">
+    <Row>
+      <Col className="post-heading">
+        <h1 className="p-2 text-white">{title}</h1>
+      </Col>
+    </Row>
+  </Container>
+);
+
+StoryViewHeaderContents.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default StoryViewHeaderContents;
