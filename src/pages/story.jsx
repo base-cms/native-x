1bbd371c10fba970eecbe72878d6e091441d52aa
Jacob Bare <jacob@limit0.io>
2018-08-15 21:47:50 -0500

    Add story metadata for twitter/opengraph/schema

--- src/pages/story.jsx
+++ src/pages/story.jsx
@@ -29,16 +29,48 @@ const Story = ({ id, preview, publisherId }) => {
             teaser,
             url,
             publisher,
+            publishedAt,
+            updatedAt,
+            primaryImage,
           } = publishedStory;
+          const { src } = primaryImage || {};
           return (
             <Fragment>
               <TrackPageView params={{ story_id: id, page_title: title }} />
               <Title value={title} />
               <Head>
+                {/* SEO */}
+                <link rel="canonical" href={url} />
                 <meta name="description" content={teaser} />
+                {src && <meta name="image" content={src} />}
+
+                {/* Schema.org */}
+                <meta item-prop="name" content={title} />
+                <meta item-prop="description" content={teaser} />
+                {src && <meta item-prop="image" content={src} />}
+
+                {/* Twitter */}
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:title" content={title} />
+                <meta name="twitter:description" content={teaser} />
+                {src && <meta name="twitter:image:src" content={src} />}
+
+                {/* OpenGraph (Facebook, etc.) */}
+                <meta name="og:title" content={title} />
+                <meta name="og:description" content={teaser} />
+                {/* @todo Create a new focal point preview for FB's 1200x630 ratio */}
+                {src && <meta name="og:image" content={src} />}
+                <meta name="og:url" content={url} />
+                <meta name="og:site_name" content={publisher.name} />
+                <meta name="og:locale" content="en_US" />
+                <meta name="og:type" content="article" />
+
+                {/* OpenGraph Article */}
+                <meta name="article:published_time" content={publishedAt} />
+                <meta name="article:modified_time" content={updatedAt} />
+
                 {/* @todo Eventually use the publisher context. */}
                 <meta name="native-x:publisher" content={publisher.name} />
-                <link rel="canonical" href={url} />
               </Head>
               <StoryView {...publishedStory} />
             </Fragment>
