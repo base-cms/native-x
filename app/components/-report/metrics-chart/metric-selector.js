7abdeb869e8a8bd98cf61b1ffadb26777e81b19e
Jacob Bare <jacob@limit0.io>
2018-08-20 16:23:57 -0500

    Update metric selector to use dropdown toggle

--- app/components/-report/metrics-chart/metric-selector.js
+++ app/components/-report/metrics-chart/metric-selector.js
@@ -1,21 +1,26 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import InitValueMixin from 'fortnight/mixins/init-value';
 import ActionMixin from 'fortnight/mixins/action';
 
 export default Component.extend(ActionMixin, InitValueMixin, {
-  selected: null,
+  selectedKey: null,
   options: null,
+
+  selected: computed('selectedKey', 'options.@each.key', function() {
+    return this.get('options').find(option => option.key === this.get('selectedKey'));
+  }),
 
   disabled: false,
 
   init() {
     this._super(...arguments);
-    this.initValue('metricOptions', []);
+    this.initValue('options', []);
   },
 
   actions: {
     setMetric(metric) {
-      this.set('selected', metric);
+      this.set('selectedKey', metric.key);
       this.sendEventAction('onchange', metric);
     }
   },
