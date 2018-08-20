16f2c2fc1ffda1f7c444d21f8873542efe3c9a85
Jacob Bare <jacob@limit0.io>
2018-08-20 12:44:17 -0500

    Create campaign metrics by day component

---
+++ app/components/-report/campaign/metrics-by-day.js
@@ -0,0 +1,85 @@
+import Component from '@ember/component';
+import ObjectQueryManager from 'ember-apollo-client/mixins/object-query-manager';
+import ActionMixin from 'fortnight/mixins/action-mixin';
+import { computed } from '@ember/object';
+import moment from 'moment';
+
+import query from 'fortnight/gql/queries/campaign/reports/by-day';
+
+export default Component.extend(ActionMixin, ObjectQueryManager, {
+  tagName: '',
+
+  startDate: computed('endDate', function() {
+    return moment(this.get('endDate')).subtract(14, 'days');
+  }),
+
+  endDate: computed(function() {
+    return moment().startOf('day');
+  }),
+
+  data: computed('rows.[]', function() {
+    const rows = this.get('rows') || [];
+    return rows.map(row => row.metrics.views);
+  }),
+
+  series: computed('data.length', function() {
+    const data = this.get('data');
+    return [{
+      name: 'Impressions',
+      data,
+    }];
+  }),
+
+  options: computed('series', 'data.length', function() {
+    const rows = this.get('rows') || [];
+    const { length } = this.get('data');
+    return {
+      chart: { type: 'areaspline' },
+      legend: { enabled: false },
+      title: { text: false },
+      xAxis: {
+        categories: rows.map(row => row.shortDate),
+        min: 0.5,
+        max: length - 1.5,
+      },
+      tooltip: {
+        formatter: function() {
+          const { index, color } = this.point;
+          const { longDate } = rows[index];
+          return `<strong>${longDate}</string><br/>
+            <span style="color:${color}">\u25CF</span> ${this.series.name}: <b>${this.y}</b>
+          `;
+        },
+      },
+      yAxis: {
+        title: { text: 'Impressions' },
+      },
+      plotOptions: {
+        areaspline: { fillOpacity: 0.5 },
+      },
+    };
+  }),
+
+  init() {
+    this._super(...arguments);
+    this.query();
+  },
+
+  async query() {
+    this.startAction();
+    const variables = {
+      input: { id: this.get('campaignId') },
+      startDate: this.get('startDate').valueOf(),
+      endDate: this.get('endDate').valueOf(),
+    };
+    try {
+      const { reports } = await this.get('apollo').query({ query, variables }, 'campaign');
+      this.set('rows', reports.byDay);
+    } catch (e) {
+      this.get('graphErrors').show(e);
+    } finally {
+      this.endAction();
+    }
+  },
+
+});
