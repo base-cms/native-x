cc4dd407f1e3f6d9cb94743397c46b7d6d2cb077
Jacob Bare <jacob@limit0.io>
2018-08-21 14:25:03 -0500

    Move query outside of campaign metrics component

--- app/components/-report/campaign/metrics.js
+++ app/components/-report/campaign/metrics.js
@@ -1,26 +1,10 @@
 import Component from '@ember/component';
-import ObjectQueryManager from 'ember-apollo-client/mixins/object-query-manager';
-import ActionMixin from 'fortnight/mixins/action-mixin';
+import ActionMixin from 'fortnight/mixins/action';
 
-import query from 'fortnight/gql/queries/campaign/metrics';
+export default Component.extend(ActionMixin, {
+  isLoading: false,
 
-export default Component.extend(ActionMixin, ObjectQueryManager, {
   didInsertElement() {
-    this.query();
+    this.sendEventAction('oninsert', this);
   },
-
-  async query() {
-    this.startAction();
-    const input = { hash: this.get('hash'), advertiserId: this.get('advertiserId') };
-    const variables = { input };
-    try {
-      const { metrics } = await this.get('apollo').query({ query, variables }, 'campaignHash');
-      this.set('metrics', metrics);
-    } catch (e) {
-      this.get('graphErrors').show(e);
-    } finally {
-      this.endAction();
-    }
-  },
-
 });
--- app/controllers/portal/campaigns/manage/report/summary.js
+++ app/controllers/portal/campaigns/manage/report/summary.js
@@ -1,10 +1,14 @@
 import Controller from '@ember/controller';
 import { inject } from '@ember/service';
 
-import query from 'fortnight/gql/queries/campaign/reports/by-day';
+import reportByDay from 'fortnight/gql/queries/campaign/reports/by-day';
+import campaignMetrics from 'fortnight/gql/queries/campaign/metrics';
 
 export default Controller.extend({
   apollo: inject(),
+
+  isReportRunning: false,
+  areMetricsLoading: false,
 
   actions: {
     async runByDayReport({ startDate, endDate }) {
@@ -15,12 +19,27 @@ export default Controller.extend({
         endDate: endDate.startOf('day').valueOf(),
       };
       try {
-        const { reports } = await this.get('apollo').query({ query, variables }, 'campaignHash');
+        const { reports } = await this.get('apollo').query({ query: reportByDay, variables }, 'campaignHash');
         this.set('rows', reports.byDay);
       } catch (e) {
         this.get('graphErrors').show(e);
       } finally {
         this.set('isReportRunning', false);
+      }
+    },
+
+    async retrieveCampaignMetrics() {
+      this.set('areMetricsLoading', true);
+      const variables = {
+        input: { hash: this.get('model.campaign.hash'), advertiserId: this.get('model.advertiser.id') },
+      };
+      try {
+        const { metrics } = await this.get('apollo').query({ query: campaignMetrics, variables }, 'campaignHash');
+        this.set('metrics', metrics);
+      } catch (e) {
+        this.get('graphErrors').show(e);
+      } finally {
+        this.set('areMetricsLoading', false);
       }
     },
   },
