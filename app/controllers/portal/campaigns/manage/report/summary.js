f15d657c576c3dfd584d2ee8474424b885438288
Jacob Bare <jacob@limit0.io>
2018-08-21 11:42:52 -0500

    Update campaign summary to use metrics chart

--- app/controllers/portal/campaigns/manage/report/summary.js
+++ app/controllers/portal/campaigns/manage/report/summary.js
@@ -1,25 +1,54 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
-import ImpressionDataMixin from 'fortnight/mixins/impression-data-mixin';
+import { inject } from '@ember/service';
+import moment from 'moment'
 
-export default Controller.extend(ImpressionDataMixin, {
+import query from 'fortnight/gql/queries/campaign/reports/by-day';
 
-  impressionSummaryTimeSeries: computed('model.days.[]', function() {
-    const type = 'line';
-    const name = 'Daily Impressions';
-    const data = this.get('model.days').map((d) => {
-      return { x: d.date, y: d.views };
-    });
-    return [{ type, name, data }];
+export default Controller.extend({
+  apollo: inject(),
+
+  metricKey: 'ctr',
+  metricOptions: null,
+  isReportRunning: false,
+
+  selectedMetric: computed('metricKey', 'metricOptions', function() {
+    return this.get('metricOptions').find(opt => opt.key === this.get('metricKey'));
   }),
 
-  ctrSummaryTimeSeries: computed('model.days.[]', function() {
-    const type = 'line';
-    const name = 'Daily CTR';
-    const data = this.get('model.days').map((d) => {
-      return { x: d.date, y: d.ctr };
-    });
-    return [{ type, name, data }];
+  startDate: computed('endDate', function() {
+    return moment(this.get('endDate')).subtract(7, 'days');
   }),
 
+  endDate: computed(function() {
+    return moment();
+  }),
+
+  init() {
+    this._super(...arguments);
+    this.set('metricOptions', [
+      { key: 'views', label: 'Impressions', tooltipFormat: '0,0', labelFormat: '0.[0]a' },
+      { key: 'clicks', label: 'Clicks', tooltipFormat: '0,0', labelFormat: '0.[0]a' },
+      { key: 'ctr', label: 'CTR', tooltipFormat: '0.[000]%', labelFormat: '0.[000]%' },
+    ]);
+  },
+
+  actions: {
+    async runByDayReport({ startDate, endDate }) {
+      this.set('isReportRunning', true);
+      const variables = {
+        input: { hash: this.get('model.campaign.hash'), advertiserId: this.get('model.advertiser.id') },
+        startDate: startDate.startOf('day').valueOf(),
+        endDate: endDate.startOf('day').valueOf(),
+      };
+      try {
+        const { reports } = await this.get('apollo').query({ query, variables }, 'campaignHash');
+        this.set('rows', reports.byDay);
+      } catch (e) {
+        this.get('graphErrors').show(e);
+      } finally {
+        this.set('isReportRunning', false);
+      }
+    },
+  },
 });
