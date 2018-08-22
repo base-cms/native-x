cbe563f0091aee5d381662875139748599665870
Jacob Bare <jacob@limit0.io>
2018-08-22 11:36:25 -0500

    Create initial dashboard publisher breakouts

--- app/components/dashboard/publisher-breakouts.js
+++ app/components/dashboard/publisher-breakouts.js
@@ -1,18 +1,57 @@
 import Component from '@ember/component';
 import ComponentQueryManager from 'ember-apollo-client/mixins/component-query-manager';
 import { getObservable } from 'ember-apollo-client';
+import moment from 'moment';
 
 import query from 'fortnight/gql/queries/publisher/dashboard';
 
+
 export default Component.extend(ComponentQueryManager, {
   classNames: ['card', 'border-0', 'z-depth-half'],
-  start: null,
-  end: null,
 
-  isLoading: true,
+  /**
+   * The chart start date.
+   *
+   * @type {Date}
+   */
+  startDate: null,
+
+  /**
+   * The chart end date.
+   *
+   * @type {Date}
+   */
+  endDate: null,
+
+  /**
+   * Whether data for the chart is being loaded.
+   *
+   * @type {boolean}
+   */
+  isLoading: false,
+
+  init() {
+    this._super(...arguments);
+    // Set initial dates.
+    const now = moment().startOf('day');
+    this.set('endDate', now);
+    this.set('startDate', moment(now).subtract(14, 'days'));
+  },
+
+  actions: {
+    setDates({ start, end }) {
+      console.info('setDates', start, end);
+      this.set('startDate', moment(start).startOf('day'));
+      this.set('endDate', moment(end).startOf('day'));
+    },
+  },
+
+
+  ////////////////
+
 
   didInsertElement() {
-    this.loadData();
+    // this.loadData();
   },
 
   async loadData() {
